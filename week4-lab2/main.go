a9308de40d86778679b4059af6082bc62a221bfe
kittiphonjaree <jareeburanapab_k@su.ac.th>
2025-08-20 05:22:11 +0000

    week5-lab

---
+++ week4-lab2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+// var email string = "jareeburanapab_k@silapkorn.edu"
+
+func main() {
+	// var name string = "kittiphon"
+	var age int = 20
+
+	email := "jareeburanapab_k@silpakorn.edu"
+	gpa := 3.75
+
+	firstname, lastname := "kittiphon", "jareeburanapab"
+
+	fmt.Printf("Name %s %s,age %d, email %s,gpa %.2f\n", firstname, lastname, age, email, gpa)
+
+}
